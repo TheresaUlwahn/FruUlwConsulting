ff964b6bf402389c0292de9a2add137aa47e32c1
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-05-04 17:31:10 +0200

    transparent to solid  navbar on place with slow scroll and sliding from the side hamburger menu

---
+++ code/main.js
@@ -0,0 +1,41 @@
+// Head navbar going from transparent to solid
+window.addEventListener("scroll", function(){
+    let menuArea = this.document.getElementById('menu-area')
+
+    if(window.pageYOffset > 0){
+        menuArea.classList.add("cus-nav")
+    }else{
+        menuArea.classList.remove("cus-nav")
+    }
+})
+
+
+// Side navbar pushing out from the right
+function openNav() {
+  document.getElementById("mySidenav").style.width = "197px";
+  // document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("main").style.marginRight = "197px";
+  document.getElementById("home").style.marginRight = "197px";
+  document.getElementById("contactMe").style.marginRight = "197px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+  document.getElementById("home").style.marginRight= "0";
+  document.getElementById("contactMe").style.marginRight= "0";
+}
+
+// Button for opening the side navigation in mobile mode
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
